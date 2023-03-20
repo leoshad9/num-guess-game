f43f45c661342ef24e84a09bca3f812a7903363b
Mohammad Shadman <leoshad9@gmail.com>
2023-03-20 20:28:44 +0530

    Update num_guess_game.m

--- num_guess_game.m
+++ num_guess_game.m
@@ -4,26 +4,19 @@
 clc;
 clear;
 x=randi([1,10]);
-y=input('Guess a number between 1-10= ');
-if          y>10 || y<1
-            fprintf("Please choose the number between 1-10!\n")  
- elseif     y==x
-            fprintf("Bingo! You guessed it right!\n");
- elseif    (y>x)
-            fprintf("Wrong guess! It's Larger\n");
- elseif     y<x
-            fprintf("Wrong! It's Smaller!\n");                    
-end
- while     (y~=x)
-            y=input('Try again= ');
-  if        y>10 || y<1
-            fprintf("Please choose the number between 1-10!\n")  
-   elseif   y==x
-            fprintf("Bingo! You guessed it right!\n"); 
-   elseif   y>x
-            fprintf("Wrong guess! It's Larger!\n");
-   elseif   y<x
-            fprintf("Wrong! It's Smaller!\n");
-  end
- end
-            fprintf("The number was= %i!\n",x);
+y=0;
+
+   while (y~=x)
+       y=input('Guess a number between 1-10= ');
+       if       y>10 || y<1
+            fprintf('Please choose the number between 1-10!\n') 
+       elseif   y==x
+            fprintf('BINGO!!\n');
+       elseif   y<x
+            fprintf('Wrong guess! Its Larger!\n');
+       elseif   y>x
+            fprintf('Wrong! Its Smaller!\n');
+       end
+          
+   end
+   
